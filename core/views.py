22c5e5cefdbe1632787c1ee160567726de662336
Jean Claude Adams <tech@arrobasoft.net>
2020-04-18 16:08:14 -0400

    Fix response status

--- core/views.py
+++ core/views.py
@@ -21,14 +21,18 @@ def post_request(request):
         # post = get_object_or_404(Post, cic=cic)
         post_set = Post.objects.filter(cic=cic)
 
-        for result in post_set:
-            fullname = result.fullname
-            social_code += SocialProgram.objects.get(code=result.social_code).description + ' '
+        if post_set.count() > 0:
+            for result in post_set:
+                fullname = result.fullname
+                social_code += SocialProgram.objects.get(code=result.social_code).description + ' '
 
-        response_data['cic'] = cic
-        response_data['fullname'] = fullname
-        response_data['social_code'] = social_code
+            response_data['cic'] = cic
+            response_data['fullname'] = fullname
+            response_data['social_code'] = social_code
+            status = 200
+        else:
+            status = 404
 
-        return JsonResponse(response_data)
+        return JsonResponse(response_data, status=status)
 
     return render(request, 'core/post_list.html', {'posts': posts})
